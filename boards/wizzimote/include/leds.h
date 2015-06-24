8e9daf479c797048eaeb836be29ba159dd44de84
ximus <maximeliron@gmail.com>
2015-06-23 18:21:14 -0700

    Fix new leds helper

--- boards/wizzimote/include/leds.h
+++ boards/wizzimote/include/leds.h
@@ -1,6 +1,8 @@
 #ifndef LEDS_H__
 #define LEDS_H__
 
+#include <msp430.h>
+
 #define LED_GREEN_ON  (P3OUT |= BIT6)
 #define LED_GREEN_OFF (P3OUT &= ~BIT6)
 
@@ -10,7 +12,7 @@
 #define LED_YELLOW_ON  (P3OUT |= BIT7)
 #define LED_YELLOW_OFF (P3OUT &= ~BIT7)
 
-static inline leds_init(void)
+static inline void leds_init(void)
 {
     // green led
     P3DIR |= BIT6;
