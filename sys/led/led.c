6f19fc28cac70b3efb9fcd4ba8ed865b3f9aec7a
ximus <maximeliron@gmail.com>
2015-07-07 22:26:54 -0700

    Introduce led module

---
+++ sys/led/led.c
@@ -0,0 +1,162 @@
+#include "thread.h"
+#include "led.h"
+
+/**
+ * some calls to `apply_state` could be avoided, but it shouldn't be
+ * an expensive operation, let's keep the code clearer.
+ */
+
+struct
+{
+    kernel_pid_t pid;
+    client_t *prev; /* linked list */
+    uint8_t state;
+}
+client_t;
+
+static client_t clients[LED_CLIENTS_MAX];
+static client_t *cur_client = NULL;
+
+
+extern void led_arch_apply_state(uint8_t);
+#define apply_state led_arch_apply_state
+
+static client_t *find_client(void)
+{
+    kernel_pid_t me = thread_getpid();
+
+    /* for every client */
+    for (int i = 0; i < LED_CLIENTS_MAX; ++i)
+    {
+        if (clients[i].pid == me)
+        {
+            return &clients[i];
+        }
+    }
+    return NULL;
+}
+
+static client_t *new_client(void)
+{
+    /* for every client */
+    for (int i = 0; i < LED_CLIENTS_MAX; ++i)
+    {
+        if (clients[i].pid == KERNEL_PID_UNDEF &&
+            clients[i].prev == NULL)
+        {
+            clients[i].pid = thread_getpid();
+            return &clients[i];
+        }
+    }
+    return NULL;
+}
+
+/**
+ * walk up the list of clients and set `cur_client` (the list head) to
+ * the first client that isn't released (still has pid set).
+ */
+static int cleanup_clients(void)
+{
+    if (cur_client == NULL)
+    {
+        return -1;
+    }
+
+    client_t *client = cur_client;
+
+    /* walk up the client aquire history */
+    while (client != NULL)
+    {
+        if (client->pid != KERNEL_PID_UNDEF)
+        {
+            break;
+        }
+        client = client->prev;
+    }
+
+    cur_client = client;
+
+    if (cur_client != NULL)
+    {
+        apply_state(cur_client->state);
+    }
+
+    return 0;
+}
+
+void led_init(void)
+{
+    for (int i = 0; i < LED_CLIENTS_MAX; ++i)
+    {
+        clients[i].pid   = KERNEL_PID_UNDEF;
+        clients[i].prev  = NULL;
+        clients[i].state = 0;
+    }
+}
+
+int led_aquire(void)
+{
+    client_t *client = find_client();
+
+    if (client == NULL)
+    {
+        client = new_client();
+    }
+
+    if (client == NULL)
+    {
+        /* no client slots left */
+        return -1;
+    }
+
+    /* NOTE: this should be an atomic operation */
+    client->prev = cur_client;
+    cur_client = client;
+}
+
+int led_release(void)
+{
+    client_t *client = find_client();
+
+    if (client == NULL)
+    {
+        return -1;
+    }
+
+    client.pid = NULL;
+
+    /**
+     * if the client is the list head, then this should be an
+     * opportunity to reduce the list size
+     */
+    if (cur_client == client)
+    {
+        cleanup_clients();
+    }
+}
+
+int led_on(uint8_t leds)
+{
+    client_t *client = find_client();
+
+    if (client == NULL)
+    {
+        return -1;
+    }
+
+    client->state |= leds;
+    apply_state(client->state);
+}
+
+int led_off(uint8_t leds)
+{
+    client_t *client = find_client();
+
+    if (client == NULL)
+    {
+        return -1;
+    }
+
+    client->state &= ~leds;
+    apply_state(client->state);
+}
---
+++ sys/led/led.h
@@ -0,0 +1,38 @@
+#ifndef LED_H__
+#define LED_H__
+
+/**
+ * Helper library to deal with concurrent access to hardware LEDs.
+ *
+ * Threads call `led_aquire` to take ownership of the array of LEDs and then
+ * `led_release` when they are done using the LEDs. Multiple threads can call
+ * `led_aquire`, the last one to call it has control of the LEDs. The state of
+ * the LEDs for each thread is remembered and restored when control of the LEDs
+ * is restored once later threads have released the LEDs. Threads can continue
+ * to alter their LED states while they are not in control of the LEDs.
+ *
+ * All calls are scoped impliclityly to current thread's PID. Maybe it would be
+ * better to provide an explicit token to represent scope (TODO).
+ *
+ * LEDs state is represented by an integer, currently 8-bit. Integer masks are
+ * passed to the led control functions (currently just `led_on` and `led_off`).
+ * Those masks toggle the bits of the state integer. When the state changes, it
+ * is passed to a board specific function `led_arch_apply_state` responsible
+ * for making sense of the state and controlling the hardware leds.
+ */
+
+#include "stdint.h"
+
+#ifndef LED_CLIENTS_MAX
+#define LED_CLIENTS_MAX (3)
+#endif
+
+void led_init(void);
+
+int led_aquire(void);
+int led_release(void);
+
+int led_on(uint8_t);
+int led_off(uint8_t);
+
+#endif /* end of include guard: LED_H__ */
